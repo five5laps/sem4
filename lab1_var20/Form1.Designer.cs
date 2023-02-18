7642109c55f506122b51fcea173fa42627bf847b
five5laps <68498669+five5laps@users.noreply.github.com>
2023-02-18 16:35:44 +0300

    Lab 1 and Lab2 combined

---
+++ lab1_var20/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace lab1_var20
+{
+    partial class OOP
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RmbStateLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RmbStateLabel
+            // 
+            this.RmbStateLabel.AutoSize = true;
+            this.RmbStateLabel.Location = new System.Drawing.Point(12, 34);
+            this.RmbStateLabel.Name = "RmbStateLabel";
+            this.RmbStateLabel.Size = new System.Drawing.Size(115, 15);
+            this.RmbStateLabel.TabIndex = 0;
+            this.RmbStateLabel.Text = "RMB NOT DETECTED";
+            // 
+            // OOP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 88);
+            this.Controls.Add(this.RmbStateLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "OOP";
+            this.Text = "Form1";
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RmbStateLabel;
+    }
+}
+
---
+++ lab1_var20/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_var20
+{
+    public partial class OOP : Form
+    {
+        public OOP()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                //lab1?
+                RmbStateLabel.Text = "RMB RELEASED";
+                //lab2?
+                MessageBox.Show("You released RMB", "Notification");
+            }
+        }
+
+    }
+}
